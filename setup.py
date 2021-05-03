59dbb8653dafbe3de551bd90cd852219109e887e
Juan Camilo Montaña Granados <jcamilo.once@gmail.com>
2021-05-03 18:41:18 -0500

    update requires

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import find_packages, setup
 INSTALL_REQUIRES = [
     "pandas==0.24.2",
     "scikit-learn==0.20.3",
-    "mlxtend==0.18.0"
+    "mlxtend==0.18.0",
+    "numpy==1.16.2"
 ]
 
 setup(
